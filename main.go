0da182cf32cf8de65f2093599b737f98075c64a9
myacoubi <amine.yacoubi.med@gmail.com>
2025-01-09 04:13:28 +0100

    DFS

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 type Room struct {
 	links []string
 	isChecked bool
-	pathIndex int
 }
 
 var (
@@ -19,13 +18,20 @@ var (
 	Rooms = make(map[string]Room)
 	start = ""
 	end = ""
+	validPaths = [][]string{}
 )
 
 func main() {
 	dataBytes, _ := os.ReadFile("data2.txt")
+	
+	// Normal BFS
 	ParsingData(string(dataBytes))
-	fmt.Println(Rooms)
+	fmt.Println(Rooms, len(Rooms))
 	FindPaths()
+	
+	// Reversed BFS
+
+
 }
 
 func FindPaths() {
@@ -33,15 +39,14 @@ func FindPaths() {
 	startRoom.isChecked = true
 	Rooms[start] = startRoom
 	paths := [][]string{{start}}
-	validPaths := [][]string{}
 	validLinks := 0
-	needToBreak := false
 	for len(paths) != 0 {
 		for i:= 0; i < len(paths); i++ {
 			currentPath := paths[i]
 			lastInPath := paths[i][len(paths[i])-1]
 
-			for j, link := range Rooms[lastInPath].links {
+			linksLoop: for j, link := range Rooms[lastInPath].links {
+
 				// fmt.Println("paths", paths)
 				if link != end {
 
@@ -68,18 +73,13 @@ func FindPaths() {
 				} else {
 					paths[i] = append(paths[i], link)
 					validPaths = append(validPaths, paths[i])
-					fmt.Println(validPaths)
+					// fmt.Println(validPaths)
 					paths = [][]string{{start}}
 					resetIsChecked(validPaths)
-					needToBreak = true
-					break
+					break linksLoop
 				}
 			}
 			validLinks = 0
-			if needToBreak {
-				needToBreak = false
-				break
-			}
 		}
 	}
 	fmt.Println("Valid paths :", validPaths)
@@ -121,10 +121,10 @@ func ParsingData(str string) any {
 		} else if  split[i] == "##end" {
 			end = strings.Split(split[i+1], " ")[0]
 			i++
-		} else if split[i][0] == '#' {
+		} else if strings.HasPrefix(split[i], "#") {
 			continue
 		} else if len(strings.Split(split[i], "-")) == 2 {
-			fillRoom(split[i])
+			fillRoomData(split[i])
 		} else {
 			log.Fatal("Errorrr")
 		}
@@ -133,9 +133,13 @@ func ParsingData(str string) any {
 	return nil
 }
 
-func fillRoom(str string) error {
+func fillRoomData(str string) error {
 	split := strings.Split(str, "-")
 
+	if (split[0] == start && split[1] == end) || (split[0] == end && split[1] == start) {
+		validPaths = [][]string{{start, end}}
+		return nil
+	}
 
 	room := Rooms[split[0]]
 	room.links = append(room.links, split[1])
